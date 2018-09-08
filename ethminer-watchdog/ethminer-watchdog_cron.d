0fa78e5e8bef9736113d0f57ee6f69f4ece442b5
Vincent Fortier <th0ma7@gmail.com>
2018-09-08 10:08:22 -0400

    Removed --HWMON and user generic <user>

--- ethminer-watchdog/ethminer-watchdog_cron.d
+++ ethminer-watchdog/ethminer-watchdog_cron.d
@@ -2,4 +2,5 @@
 # cron.d/ethminer-watchdog -- ethminer & AMD GPU Watchdog
 #
 # By default, run every 5 minutes
-*/5 * * * * th0ma7 [ -x /usr/local/bin/ethminer-watchdog.bash ] && /usr/local/bin/ethminer-watchdog.bash --HWMON >> /var/log/miners/ethminer-watchdog.log 2>&1
+#*/5 * * * * <user> [ -x /usr/local/bin/ethminer-watchdog.bash ] && /usr/local/bin/ethminer-watchdog.bash --noact >> /var/log/miners/ethminer-watchdog.log 2>&1
+*/5 * * * * <user> [ -x /usr/local/bin/ethminer-watchdog.bash ] && /usr/local/bin/ethminer-watchdog.bash >> /var/log/miners/ethminer-watchdog.log 2>&1
